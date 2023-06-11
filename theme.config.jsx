ed390069d44a10e8f901dea782499b8c28db13cd
huraji <huraji.art@gmail.com>
2023-06-11 16:51:14 +0200

    3 - add footer credits and format

--- theme.config.jsx
+++ theme.config.jsx
@@ -10,19 +10,26 @@ export default {
     },
     footer: {
       text: (
-        <span>
-          {new Date().getFullYear()} ©{' '}
-          <a href="/licenses/ai-restrictions-for-creatives">
-            {'Under AIR-C License'}
-          </a>
-          {' | '}
-          <a href="/cookie-consent">
-            {'Cookie Consent'}
-          </a>
-          {' | '}
-          <a href="/privacy-policy">
-            {'Privacy Policy'}
-          </a>
+        <span style={{ fontSize: '12px', display: 'flex', flexDirection: 'column', flex: 1, alignItems: 'center' }}>
+          <span>
+            {new Date().getFullYear()} ©{' '}
+            <a href="/licenses/ai-restrictions-for-creatives">
+              {'Under AIR-C License'}
+            </a>
+            {' - '}
+            <a href="https://nextra.site/" target="_blank">
+              {'Built with Nextra ❤️'}
+            </a>
+          </span>
+          <span>
+            <a href="/cookie-consent">
+                {'Cookie Consent'}
+              </a>
+              {' | '}
+              <a href="/privacy-policy">
+                {'Privacy Policy'}
+              </a>
+          </span>
         </span>
       )
     },
