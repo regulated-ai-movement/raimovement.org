cca483b216a4ed8bba892c79e03dcad407644519
huraji <huraji.art@gmail.com>
2023-06-18 19:13:49 +0200

    feat: added navbar extra content for social icons - changed README

---
+++ shared/config.ts
@@ -0,0 +1,6 @@
+export enum NavigationIconLinks {
+    LinkedIn = 'https://www.linkedin.com/company/regulated-ai-movement/',
+    Twitter = 'https://twitter.com/raimovement',
+    GitHub = 'https://github.com/regulated-ai-movement/raimovement.org',
+    Discord = 'https://discord.gg/tckpz2scJw'
+}
