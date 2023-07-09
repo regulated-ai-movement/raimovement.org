ce3eadd02b20785ba684c8655f4c635fcd13dd63
huraji <huraji.art@gmail.com>
2023-07-09 20:15:57 +0200

    add: (4) add logo pack / minor text fixes

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,9 +1,22 @@
 import { useRouter } from 'next/router'
 import { NavbarExtraContent } from './components/navbar-extra-content'
-import { NavigationIconLinks } from './shared/config'
+import { MainLogo } from './components/main-logo'
 
 export default {
-    logo: <span>{'The Regulated AI Movement'}</span>,
+    head: () => {
+      return (
+        <>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </>
+      )
+    },
+    logo: (<MainLogo width={250} title={'The Regulated AI Movement'} />),
     navbar: {
       extraContent: (<NavbarExtraContent />),
     },
