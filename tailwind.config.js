c68618aeee3db7e95a7fbe2364253707a874813c
DeBoni <emanuele.deboni@adidas.com>
2023-11-22 09:08:55 +0100

    feat (#7): add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'theme-blue': '#1a2e45',
+        'theme-white': '#f1f5f9',
+        'theme-red': '#e60039',
+      },
+    },
+  },
+  plugins: [],
+}
